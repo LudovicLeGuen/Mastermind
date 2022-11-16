d71012502c12e2cf9872c92c89bdd732aa2ec025
LudovicLeGuen <ludovicleguen@me.com>
2022-11-16 16:36:20 +0000

    modify JS and HTML to create rows

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,5 @@
-
-function duplicateChildNodes (parentId){
-        var parent = document.getElementById(parentId);
-        NodeList.prototype.forEach = Array.prototype.forEach;
-        var children = parent.childNodes;
-        children.forEach(function(item){
-          var cln = item.cloneNode(true);
-          parent.appendChild(cln);
-        });
-      };
-      
-      duplicateChildNodes("decoding-board");
+for (let i = 0; i < 11; i++) {
+        let row = document.createElement('div');
+        row.classList.add("row")
+        document.getElementById('decoding-board').appendChild(row);
+}
