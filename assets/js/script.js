8d8b7d6554db2dc37ce3f9c0c1f9619ef1dd2905
LudovicLeGuen <ludovicleguen@me.com>
2022-11-17 15:34:10 +0000

    add basic JS structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,16 @@
-for (let i = 0; i < 11; i++) {
-        let row = document.createElement('div');
-        row.classList.add("row")
-        document.getElementById('decoding-board').appendChild(row);
+function runGame(){
+
 }
+
+function pickColor(){
+
+}
+
+function compareCode(){
+
+}
+
+function pegCreate(){
+
+}
+
