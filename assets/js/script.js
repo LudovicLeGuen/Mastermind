1c60ae65a32a9417498feb6238a559245f756fa9
LudovicLeGuen <ludovicleguen@me.com>
2022-11-17 19:34:22 +0000

    add random color code function on JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,35 @@
-function runGame(){
+function runGame() {
+
+}
+/**
+ * The secret code of 4 colors is randomly created
+ * and the users will have to guess it.
+ */
+const colors = ["blue", "red", "yellow", "green", "orange", "purple"];
+let final = randomCode()
+
+// Creates 4 colors randomly
+function randomCode() {
+        const randomFinal = [...new Array(4)].map(color => {
+                const random = Math.floor(Math.random() * Math.floor(colors.length));
+                return colors[random];
+        });
+        console.log(randomFinal);
+        return randomFinal;
+}
+
+function pickColor() {
 
 }
 
-function pickColor(){
+function compareCode() {
 
 }
 
-function compareCode(){
+function pegCreate() {
 
 }
 
-function pegCreate(){
-
-}
-
-function incrementRow(){
+function incrementRow() {
 
 }
