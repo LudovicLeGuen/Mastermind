5286baabe3e5a07c1045a74c78fcb87886112b38
LudovicLeGuen <ludovicleguen@me.com>
2022-11-18 10:54:53 +0000

    add event listener on colors and place holders on board

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,8 @@ function runGame() {
  * The secret code of 4 colors is randomly created
  * and the users will have to guess it.
  */
-const colors = ["blue", "red", "yellow", "green", "orange", "purple"];
-let final = randomCode()
+let colors = ["blue", "red", "yellow", "green", "orange", "purple"];
+let secretCode = randomCode()
 
 // Creates 4 colors randomly
 function randomCode() {
@@ -17,6 +17,32 @@ function randomCode() {
         console.log(randomFinal);
         return randomFinal;
 }
+
+/**
+ * User selects the spot where he wants to 
+ * place a color.
+ */
+let holders = document.getElementsByClassName('color-holder');
+
+for (let holder of holders) {
+        holder.addEventListener('click', function handleClick(event) {
+                console.log('box clicked', event);
+                holder.setAttribute('style', 'background-color: yellow;');
+        });
+}
+/**
+ * User selects the color he wants to  
+ * place on the selected spot.
+ */
+ let choices = document.getElementsByClassName('color');
+
+ for (let choice of choices) {
+         choice.addEventListener('click', function handleClick(event) {
+                 console.log('color chosen', event);
+                 holder.setAttribute('style', 'border: solid 2px pink;');
+         });
+ }
+
 
 function pickColor() {
 
