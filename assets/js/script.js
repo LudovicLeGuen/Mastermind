a4d7ad34c438d48cbdaad6572e5ca15ac36ef95d
LudovicLeGuen <ludovicleguen@me.com>
2022-11-18 15:18:49 +0000

    change html layout of the board and modify css accordingly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,9 @@
-function runGame() {
 
-}
 /**
  * The secret code of 4 colors is randomly created
  * and the users will have to guess it.
  */
-let colors = ["blue", "red", "yellow", "green", "orange", "purple"];
-let secretCode = randomCode()
+
 
 // Creates 4 colors randomly
 function randomCode() {
@@ -28,21 +25,22 @@ for (let holder of holders) {
         holder.addEventListener('click', function handleClick(event) {
                 console.log('box clicked', event);
                 holder.setAttribute('style', 'background-color: yellow;');
+
         });
 }
 /**
  * User selects the color he wants to  
  * place on the selected spot.
  */
- let choices = document.getElementsByClassName('color');
 
- for (let choice of choices) {
-         choice.addEventListener('click', function handleClick(event) {
-                 console.log('color chosen', event);
-                 holder.setAttribute('style', 'border: solid 2px pink;');
-         });
- }
+let choices = document.getElementsByClassName('color');
 
+for (let choice of choices) {
+        choice.addEventListener('click', function onClick(event) {
+                console.log('color chosen', event);
+                document.cell.style.backgroundColor = 'green';
+        });
+}
 
 function pickColor() {
 
