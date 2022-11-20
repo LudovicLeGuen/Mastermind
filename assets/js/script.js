a008021e3458d7c4d0152532ebaa85d421071031
LudovicLeGuen <ludovicleguen@me.com>
2022-11-20 09:39:07 +0000

    add minor changes to HTML CSS and JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,33 @@
-
 /**
  * The secret code of 4 colors is randomly created
  * and the users will have to guess it.
  */
-
-
-// Creates 4 colors randomly
-function randomCode() {
-        const randomFinal = [...new Array(4)].map(color => {
-                const random = Math.floor(Math.random() * Math.floor(colors.length));
-                return colors[random];
-        });
-        console.log(randomFinal);
-        return randomFinal;
-}
-
+ const colors = ["blue", "red", "yellow", "green", "orange", "purple"];
+ let final = randomCode()
+ 
+ // Creates 4 colors randomly
+ function randomCode() {
+         const randomFinal = [...new Array(4)].map(color => {
+                 const random = Math.floor(Math.random() * Math.floor(colors.length));
+                 return colors[random];
+         });
+         console.log(randomFinal);
+         return randomFinal;
+ }
 /**
  * User selects the spot where he wants to 
  * place a color.
  */
+
 let holders = document.getElementsByClassName('color-holder');
 
 for (let holder of holders) {
         holder.addEventListener('click', function handleClick(event) {
                 console.log('box clicked', event);
-                holder.setAttribute('style', 'background-color: yellow;');
-
+                holder.style.backgroundColor = "white";
         });
 }
+
 /**
  * User selects the color he wants to  
  * place on the selected spot.
@@ -38,7 +38,7 @@ let choices = document.getElementsByClassName('color');
 for (let choice of choices) {
         choice.addEventListener('click', function onClick(event) {
                 console.log('color chosen', event);
-                document.cell.style.backgroundColor = 'green';
+                choice.style.borderColor = "white";
         });
 }
 
