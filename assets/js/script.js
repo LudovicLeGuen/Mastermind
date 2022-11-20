4310370ddae012815a68521904ff600cbb9273f0
LudovicLeGuen <ludovicleguen@me.com>
2022-11-20 17:59:17 +0000

    add event listener to toggle colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,61 @@
+var guessings = {
+        "row1": [],
+        "row2": [],
+        "row3": [],
+        "row4": [],
+        "row5": [],
+        "row6": [],
+        "row7": [],
+        "row8": [],
+        "row9": [],
+        "row10": [],
+
+}
+
 /**
  * The secret code of 4 colors is randomly created
  * and the users will have to guess it.
  */
- const colors = ["blue", "red", "yellow", "green", "orange", "purple"];
- let final = randomCode()
- 
- // Creates 4 colors randomly
- function randomCode() {
-         const randomFinal = [...new Array(4)].map(color => {
-                 const random = Math.floor(Math.random() * Math.floor(colors.length));
-                 return colors[random];
-         });
-         console.log(randomFinal);
-         return randomFinal;
- }
-/**
- * User selects the spot where he wants to 
- * place a color.
- */
+const colors = ["blue", "red", "yellow", "green", "orange", "purple"];
+let final = randomCode()
 
-let holders = document.getElementsByClassName('color-holder');
-
-for (let holder of holders) {
-        holder.addEventListener('click', function handleClick(event) {
-                console.log('box clicked', event);
-                holder.style.backgroundColor = "white";
+// Creates 4 colors randomly
+function randomCode() {
+        const randomFinal = [...new Array(4)].map(color => {
+                const random = Math.floor(Math.random() * Math.floor(colors.length));
+                return colors[random];
         });
+        console.log(randomFinal);
+        return randomFinal;
 }
 
 /**
- * User selects the color he wants to  
- * place on the selected spot.
+ * User toggles colors to create a code guess.
  */
+const colorCode = document.getElementsByClassName("color-holder");
 
-let choices = document.getElementsByClassName('color');
+for (var i = 0; i < colorCode.length; i++) {
+        colorCode[i].addEventListener('click', function onClick(event) { // loop through all elements with the "color-holder" Class attribute
 
-for (let choice of choices) {
-        choice.addEventListener('click', function onClick(event) {
-                console.log('color chosen', event);
-                choice.style.borderColor = "white";
-        });
-}
+                const backgroundColor = event.target.style.backgroundColor; // event.target allows me to toggle colors only on the clicked button
 
-function pickColor() {
+                if (backgroundColor === '') {
+                        event.target.style.backgroundColor = 'red';
+                } else if (backgroundColor === 'red') {
+                        event.target.style.backgroundColor = 'yellow';
+                } else if (backgroundColor === 'yellow') {
+                        event.target.style.backgroundColor = 'green';
+                } else if (backgroundColor === 'green') {
+                        event.target.style.backgroundColor = 'orange';
+                } else if (backgroundColor === 'orange') {
+                        event.target.style.backgroundColor = 'purple';
+                } else {
+                        event.target.style.backgroundColor = 'blue';
+                };
+        })
+};
+
+function createGuesscode() {
 
 }
 
